584455958ed475da08e4b88281c82ecff064a1f1
Jesus Badillo <jbadillo2199@gmail.com>
2022-09-11 20:15:37 -0500

    added contract for whitelist

--- contracts/Clu3.sol
+++ contracts/Clu3.sol
@@ -1,9 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "./Clu3Whitelist.sol";
+
+error Clu3__TimestampPassed();
 
 contract Clu3 {
-    // Code for later
+    /**
+     *  @notice
+     *  @dev
+     */
+
+    address private immutable i_owner;
+
+    struct Clu3Data {
+        uint256 clu3Timestamp;
+        address clueAddress;
+    }
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) {
+            revert Clu3Whitelist__NotOwner();
+        }
+        _;
+    }
+
+    // Map clu3Id to
+    Clu3Whitelist private clu3Whitelist;
+
+    // function createClu3Transaction(uint256 lifespan, address clu3Address)
+    //     private
+    //     onlyOwner
+    // {
+    //     bytes32 eve = keccak256(abi.encodePacked(msg.sender, clu3Address));
+    // }
 }
