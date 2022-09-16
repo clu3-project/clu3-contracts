b72dc08902f2b11a63296632171ab084bb1bfc73
Jesus Badillo <jbadillo2199@gmail.com>
2022-09-15 20:05:15 -0500

    added verification to clu3 contracts

--- contracts/Clu3.sol
+++ contracts/Clu3.sol
@@ -2,40 +2,97 @@
 pragma solidity ^0.8.9;
 
 import "./Clu3Whitelist.sol";
+import "./VerifySigner.sol";
 
-error Clu3__TimestampPassed();
+error Clu3__NotOwner();
 
-contract Clu3 {
+error Clu3__TimestampAlreadyPassed();
+
+error Clu3__SignerNotValid();
+
+/**
+ * @title Clu3: a smart contract to create transactions
+ * @notice
+ * @author Jesus Badillo
+ */
+contract Clu3 is VerifySignature {
     /**
      *  @notice
      *  @dev
      */
 
-    address private immutable i_owner;
-
-    struct Clu3Data {
-        uint256 clu3Timestamp;
-        address clueAddress;
+    // Enumerate the correct Web3 storage implementations
+    enum Web3StorageImplementation {
+        IPFS,
+        TABLELAND,
+        FILECOIN,
+        CERAMIC
     }
 
-    constructor() {
+    // State Variables
+    address private immutable i_signer;
+    uint256 private immutable i_lifespan;
+    address private immutable i_owner;
+    uint256 private immutable i_eventTimestamp;
+    bytes private s_clu3Id;
+
+    // Web3 Service (IPFS, Ceramic, Tableland, Filecoin)
+    Clu3Whitelist private s_clu3Whitelist;
+
+    struct Clu3Event {
+        uint256 clu3EventTimestamp;
+        address clueEventAddress;
+    }
+
+    constructor(
+        address _signer,
+        uint256 _lifespan,
+        uint256 _clu3Id
+    ) {
+        i_signer = _signer;
+        i_lifespan = _lifespan;
+        s_clu3Id = _clu3Id;
+        i_eventTimestamp = block.timestamp;
         i_owner = msg.sender;
     }
 
     modifier onlyOwner() {
         if (msg.sender != i_owner) {
-            revert Clu3Whitelist__NotOwner();
+            revert Clu3__NotOwner();
         }
         _;
     }
 
-    // Map clu3Id to
-    Clu3Whitelist private clu3Whitelist;
+    function senderInWhitelist(address _sender) public view returns (bool) {
+        if (s_clu3Whitelist.isWhitelisted(_sender)) {
+            return true;
+        }
+        return false;
+    }
 
-    // function createClu3Transaction(uint256 lifespan, address clu3Address)
-    //     private
-    //     onlyOwner
-    // {
-    //     bytes32 eve = keccak256(abi.encodePacked(msg.sender, clu3Address));
-    // }
+    function isWhitelistImplemented() public view returns (bool) {
+        if (s_clu3Whitelist.getNumberOfWhitelistedAddresses() == 0) {
+            return false;
+        }
+        return true;
+    }
+
+    function createClu3Transaction() private view returns (bool) {
+        if (!senderInWhitelist(msg.sender) || !isWhitelistImplemented()) {
+            return true;
+        }
+
+        if (i_eventTimestamp + i_lifespan > block.timestamp) {
+            revert Clu3__TimestampAlreadyPassed();
+        }
+
+        if (verifyMessage(_hashedMessage, _v, _r, _s) != i_signer) {}
+        // bytes32 eve = keccak256(
+        //     abi.encodePacked(
+        //         i_owner,
+        //         s_clu3Whitelist.s_whitelistAddresses,
+        //         lifespan
+        //     )
+        // );
+    }
 }
--- contracts/Clu3Whitelist.sol
+++ contracts/Clu3Whitelist.sol
@@ -10,8 +10,8 @@ error Clu3Whitelist__MaxWhitelistAddressesReached();
 error Clu3Whitelist__NotWhitelisted();
 
 /**
- *  @title Clu3Whitelist: A whitelist for the addresses that can mint the
- *  @author Jesus Badillo, Efrain
+ *  @title Clu3Whitelist: A whitelist for the addresses that can mint a Clu3 captcha transaction
+ *  @author Jesus Badillo
  *  @notice
  *  @dev
  */
@@ -25,8 +25,8 @@ contract Clu3Whitelist {
     // The owner of the contract
     address private immutable i_owner;
 
-    // To store our addresses, we need to create a mapping that will receive the user's address and return if he is whitelisted or not.
-    mapping(address => bool) whitelistAddresses;
+    // To store our addresses, we need to create a mapping that will receive the user's address and return if they are whitelisted or not.
+    mapping(address => bool) private s_whitelistAddresses;
 
     constructor(uint256 _maxWhitelistAddresses) {
         i_owner = msg.sender;
@@ -42,15 +42,15 @@ contract Clu3Whitelist {
     }
 
     /**
-     *  @notice function addUserAddressToWhitelist
-     *  @dev
+     *  @dev function addUserAddressToWhitelist
+     *
      */
     function addUserAddressToWhitelist(address _addressToWhitelist)
         public
         onlyOwner
     {
         // Validate the caller is not already part of the whitelist.
-        if (!whitelistAddresses[_addressToWhitelist]) {
+        if (!s_whitelistAddresses[_addressToWhitelist]) {
             revert Clu3Whitelist__AlreadyInWhitelist();
         }
 
@@ -60,7 +60,7 @@ contract Clu3Whitelist {
         }
 
         // Set whitelist boolean to true.
-        whitelistAddresses[_addressToWhitelist] = true;
+        s_whitelistAddresses[_addressToWhitelist] = true;
 
         // Increasing the count
         numberOfAddressesWhitelisted += 1;
@@ -72,7 +72,7 @@ contract Clu3Whitelist {
         returns (bool)
     {
         // Verifying if the user has been whitelisted
-        bool userIsWhitelisted = whitelistAddresses[_whitelistAddress];
+        bool userIsWhitelisted = s_whitelistAddresses[_whitelistAddress];
         return userIsWhitelisted;
     }
 
@@ -86,7 +86,7 @@ contract Clu3Whitelist {
         returns (bool)
     {
         // Verifying if the user has been whitelisted
-        return whitelistAddresses[_whitelistAddress];
+        return s_whitelistAddresses[_whitelistAddress];
     }
 
     // Remove user from whitelist
@@ -95,12 +95,12 @@ contract Clu3Whitelist {
         onlyOwner
     {
         // Validate the caller is already part of the whitelist.
-        if (whitelistAddresses[_addressToRemove]) {
+        if (s_whitelistAddresses[_addressToRemove]) {
             revert Clu3Whitelist__NotWhitelisted();
         }
 
         // Set whitelist boolean to false.
-        whitelistAddresses[_addressToRemove] = false;
+        s_whitelistAddresses[_addressToRemove] = false;
 
         // This will decrease the number of whitelisted addresses.
         numberOfAddressesWhitelisted -= 1;
